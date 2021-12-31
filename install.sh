fcbf91e0a00d5363ffaf49639ed1be24d825c1a3
mzpbvsig <972403325@qq.com>
2021-12-31 17:27:13 +0800

    full README

--- install.sh
+++ install.sh
@@ -1,16 +1,33 @@
 #! /bin/sh
 
-yum -y install gcc
-yum -y install gcc-c++
+#======================================================================#
+#  System Required:  CentOS 7 or Higher                                #
+#  GB28181+ZLMediaKit服务 一键安装与启动脚本                             #          
+#  Author：zhangkai <mzpbvsig@gmail.com>                               #
+#======================================================================#
+
+# Check command is exist
+check_command(){
+    local command=$1 
+    if hash $command 2> /dev/null; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+yum -y install gcc gcc-c++
 
 # CMake complie by source
-wget https://cmake.org/files/v3.3/cmake-3.3.2.tar.gz
-tar -xvf cmake-3.3.2.tar.gz
-cd cmake-3.3.2
-./configure
-make -j4
-make install
-cd ..
+if check_command cmake; then
+    wget https://cmake.org/files/v3.3/cmake-3.3.2.tar.gz
+    tar -xvf cmake-3.3.2.tar.gz
+    cd cmake-3.3.2
+    ./configure
+    make -j4
+    make install
+    cd ..
+fi
 
 # ZLMediaKit complie by source
 git clone --depth 1 https://gitee.com/xia-chu/ZLMediaKit
@@ -24,7 +41,8 @@ cmake ..
 make -j4
 
 # wvp complie by source
-yum -y install java git maven nodejs npm
+yum -y install java git maven nodejs 
+
 git clone https://gitee.com/pan648540858/wvp-GB28181-pro.git
 cd wvp-GB28181-pro/web_src/
 npm --registry=https://registry.npm.taobao.org install
@@ -33,7 +51,7 @@ npm run build
 cd ..
 mvn package
 
-npm --registry=https://registry.npm.taobao.org install -g pm2
+
 
 cd ..
 root=/home/wvp
@@ -41,5 +59,6 @@ cp -f wvp-pro.yml ${root}/wvp-GB28181-pro/target/application.yml
 cp -f assist.yml ${root}/wvp-pro-assist/target/application.yml
 cp -f config.ini  ${root}/ZLMediaKit/release/linux/Debug/config.ini
 
+npm --registry=https://registry.npm.taobao.org install -g pm2
 pm2 stop wvp-pro.json
 pm2 start wvp-pro.json
